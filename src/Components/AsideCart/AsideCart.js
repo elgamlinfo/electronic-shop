1a01fde766ec26ccd1cafa82255f6e0db892eef6
Mos Tafa <elgaml.info@gmail.com>
2021-10-13 15:40:13 +0200

    home v~2

--- src/Components/AsideCart/AsideCart.js
+++ src/Components/AsideCart/AsideCart.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import AsideProductCart from '../Helpers/AsideProductCart'
 import './aside-cart.scss'
+import { Scrollbars } from 'react-custom-scrollbars';
 
 const AsideCart = (props) => {
     return (
-        <div className={`aside-cart-container ${props.clicked?'active':'active'}`} >
+        <div className={`aside-cart-container ${props.clicked?'active':''}`} >
             <div className='aside-cart'>
                 <div className='close-btn'><i className='fas fa-times' onClick={ () => props.cartClickHunler() }></i></div>
                 <div className='cart-products'>
+                <Scrollbars>
+                    <AsideProductCart />
+                    <AsideProductCart />
+                    <AsideProductCart />
+                    <AsideProductCart />
+                    <AsideProductCart />
+                    <AsideProductCart />
+                </Scrollbars>
                 </div>
                 <div className='checkout'>
                     <button>checkout</button>
---
+++ src/Components/Helpers/AsideProductCart.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './asideproductcart.scss'
+import ProductImg from '../../images/product.png'
+const AsideProductCart = () => {
+    return (
+        <div className='aside-product'>
+            <div className='img-box'>
+                <img  src={ProductImg} alt=''/>
+            </div>
+            <div className='product-info'>
+                <h2 className='product-name'>MacBook Pro 16 Inch</h2>
+                <p className='product-price'>2,000,000$</p>
+                <button className='read-more'>read more</button>
+            </div>
+        </div>
+    )
+}
+
+export default AsideProductCart
