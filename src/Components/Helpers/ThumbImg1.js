f5eda32ef608a92f020ecac7f481c203dbe51f37
elgaml.info <elgaml.info@gmail.com>
2021-10-24 13:11:10 +0200

    product res

--- src/Components/Helpers/ThumbImg1.js
+++ src/Components/Helpers/ThumbImg1.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import PorductImg from '../../images/product.png'
 import './thumbimg1.scss'
 
 const ThumbImg1 = (props) => {
     return (
-        <div className='thumb_img' style={{
-                width: props.width? props.width:'70%',
-                height: props.height? props.height:'auto',
-            }}>
-            <img src={PorductImg} alt=''/>
+        <div 
+        className='thumb_img' 
+        style={{
+            width: props.width? props.width:'70%',
+            height: props.height? props.height:'auto',
+        }} 
+        >
+        {props.clickHundler?
+        <img src={props.PorductImg} alt='' onClick= {(e) => props.clickHundler(e)}/>:
+        <img src={props.PorductImg} alt=''/>}
         </div>
     )
 }
--- src/Components/ProductThumb/ProductThumb.js
+++ src/Components/ProductThumb/ProductThumb.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ThumbImg1 from '../Helpers/ThumbImg1'
+import PorductImg from '../../images/product.png'
 import './productthumb.scss'
 
 
 const ProductThumb = () => {
+    let [imgSrc, setImgSrc]= useState();
+
+    let clickHundler = (e) => {
+        console.log(e.target.src);
+        setImgSrc(e.target.src) 
+    }
     return (
         <div className='product_thumb'>
             <div className='thumb_1'>
-                <ThumbImg1 />
-                <ThumbImg1 />
-                <ThumbImg1 />
-                <ThumbImg1 />
+                <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg}/>
+                <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg}/>
+                <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg}/>
+                <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg}/>
             </div>
             <div className='thumb_2'>
-                <ThumbImg1 width='90%' height='90%'/>
+                <ThumbImg1 width='90%' height='90%' PorductImg={PorductImg}/>
             </div>
         </div>
     )
