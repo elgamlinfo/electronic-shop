1c511a62bd9e519057262954909fb1479d34b6ca
elgaml.info <elgaml.info@gmail.com>
2021-10-24 14:48:48 +0200

    LAST version of product

--- src/Components/ProductThumb/ProductThumb.js
+++ src/Components/ProductThumb/ProductThumb.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, {useState } from 'react'
 import ThumbImg1 from '../Helpers/ThumbImg1'
 import PorductImg from '../../images/product.png'
+import PorductImg2 from '../../images/product2.png'
+import PorductImg3 from '../../images/product3.png'
 import './productthumb.scss'
 
 
 const ProductThumb = () => {
-    let [imgSrc, setImgSrc]= useState();
+    let [imgSrc, setImgSrc]= useState(PorductImg);
 
     let clickHundler = (e) => {
         console.log(e.target.src);
@@ -15,12 +17,12 @@ const ProductThumb = () => {
         <div className='product_thumb'>
             <div className='thumb_1'>
                 <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg}/>
+                <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg2}/>
                 <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg}/>
-                <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg}/>
-                <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg}/>
+                <ThumbImg1 clickHundler={clickHundler} PorductImg={PorductImg3}/>
             </div>
             <div className='thumb_2'>
-                <ThumbImg1 width='90%' height='90%' PorductImg={PorductImg}/>
+                <ThumbImg1 width='90%' height='90%' PorductImg={imgSrc}/>
             </div>
         </div>
     )
