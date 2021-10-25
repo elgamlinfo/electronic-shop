7f1fd932734ea77cc1a693473a57cfe151e254fe
elgamlinfo <elgaml.info@gmail.com>
2021-10-25 21:59:33 +0200

    mobile responsive

--- src/Components/AsideFilter/AsideFilter.js
+++ src/Components/AsideFilter/AsideFilter.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Filter from '../Helpers/Filter'
 import './AsideFilter.scss'
 const AsideFilter = () => {
+    const [active, setActive] = useState('');
+
+    let toggleHundel = (e) => {
+        active === ''?setActive("active"):setActive("");
+    }
     return (
-        <div className='aside_filter'>
+        <div className={`aside_filter ${active}`}>
+            <button className='toggle_btn' onClick={(e) => toggleHundel(e)}>
+                <i className='fas fa-cog'></i>
+            </button>
             <div className='filter_wrapper'>
                 <Filter title='screen size'/>
                 <Filter title='color'/>
