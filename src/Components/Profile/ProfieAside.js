12e896e3ada6afa58ceb8c873b8bdc72d093c090
elgamlinfo <elgaml.info@gmail.com>
2021-12-11 19:15:24 +0200

    profile/profilecontent Finish

--- src/Components/Profile/ProfieAside.js
+++ src/Components/Profile/ProfieAside.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import ProfileImg from '../../images/person.webp'
 import './profile-aside.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faUser,faShoppingBag,faHeart } from '@fortawesome/free-solid-svg-icons'
 const ProfieAside = () => {
     return (
         <div className='profile_aside'>
@@ -17,11 +17,11 @@ const ProfieAside = () => {
                     <Link to='/profile'>profile</Link>
                 </div>
                 <div className='navs'>
-                    <span><FontAwesomeIcon icon={faUser}/></span>
+                    <span><FontAwesomeIcon icon={faShoppingBag}/></span>
                     <Link to='/profile/orders'>orders</Link>
                 </div>
                 <div className='navs'>
-                    <span><FontAwesomeIcon icon={faUser}/></span>
+                    <span><FontAwesomeIcon icon={faHeart}/></span>
                     <Link to='/profile/favurite'>favurite</Link>
                 </div>
             </div>
--- src/Components/Profile/ProfileContent.js
+++ src/Components/Profile/ProfileContent.js
@@ -3,7 +3,42 @@ import './profile-content.scss'
 const Profile = () => {
     return (
         <div className='profile_content'>
-            <h3 className='head'>Profile</h3>
+            <h2 className='titl'>Profile</h2>
+            <div className='profile_form_cont'>
+                <form>
+                    <div className='row'>
+                        <div className='input_group'>
+                            <label htmlFor='name'>name</label>
+                            <input type='text' name='name'id='name'/>
+                        </div>
+                        <div className='input_group'>
+                            <label htmlFor="email">email</label>
+                            <input type='email' name='email'id='email'/>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='input_group'>
+                            <label htmlFor='password'>password</label>
+                            <input type='password' name='password'id='password'/>
+                        </div>
+                        <div className='input_group'>
+                            <label htmlFor='address'>address</label>
+                            <input type='text' name='address' id='address'/>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='input_group'>
+                            <label htmlFor='mobile'>mobile</label>
+                            <input type='text' name='mobile'id='mobile'/>
+                        </div>
+                        <div className='input_group'>
+                            <label htmlFor='image'>image</label>
+                            <input type='file' name='image' id='image'/>
+                        </div>
+                    </div>
+                    <button>save</button>
+                </form>
+            </div>
         </div>
     )
 }
