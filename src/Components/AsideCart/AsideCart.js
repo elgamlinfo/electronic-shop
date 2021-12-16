0c284010ba7024c7e263482c431053a8a2784920
elgamlinfo <elgaml.info@gmail.com>
2021-12-16 14:42:45 +0200

    fix aside cart scroll bar

--- src/Components/AsideCart/AsideCart.js
+++ src/Components/AsideCart/AsideCart.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import AsideProductCart from '../Helpers/AsideProductCart'
 import './aside-cart.scss'
-import { Scrollbars } from 'react-custom-scrollbars';
 
 const AsideCart = (props) => {
     return (
@@ -9,14 +8,12 @@ const AsideCart = (props) => {
             <div className='aside-cart'>
                 <div className='close-btn'><i className='fas fa-times' onClick={ () => props.cartClickHunler() }></i></div>
                 <div className='cart-products'>
-                <Scrollbars>
                     <AsideProductCart />
                     <AsideProductCart />
                     <AsideProductCart />
                     <AsideProductCart />
                     <AsideProductCart />
                     <AsideProductCart />
-                </Scrollbars>
                 </div>
                 <div className='checkout'>
                     <button>checkout</button>
