811292d837e3f49f45a2508cfd7f24c195b7b489
elgamlinfo <elgaml.info@gmail.com>
2021-12-27 12:54:29 +0200

    change signup layout

--- src/Components/AuthForm/AuthForm.js
+++ src/Components/AuthForm/AuthForm.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import Form from './Form'
+import { Link } from 'react-router-dom'
 import './authform.scss'
+import OAuthButton from '../Helpers/OAuthButton'
 import FormValid from './FormValid'
 const AuthForm = (props) => {
-   
+    
     return (
         <div className='form'>
-            <Form data = {props.inputsData} text={props.text}/>
+            <form className='form_cont'>
+                <h2 className='form_title'>welcome to our app</h2>
+                <OAuthButton />
+                <div className='or_line'>OR</div>
+                <input 
+                    type='text' 
+                    name='name'
+                    placeholder='your name here'
+                />
+                <input 
+                    type='email' 
+                    name='email'
+                    placeholder='your email here'
+                />
+                <input 
+                    type='password' 
+                    name='password'
+                    placeholder='your password here'
+                />
+                <button className='submit'>signup</button>
+                <p className='redir'>have an account ? <Link to="/login">login</Link></p>
+            </form>
             <FormValid />
         </div>
     )
--- src/Pages/Signup/SignUp.js
+++ src/Pages/Signup/SignUp.js
@@ -3,25 +3,10 @@ import Loading from '../../Components/Loading/Loading';
 import AuthForm from '../../Components/AuthForm/AuthForm'
 import {useDispatch} from 'react-redux'
 import {footerActions} from '../../Store/FooterSlice'
+
+
 const SignUp = () => {
     const dispatch = useDispatch();
-    let inputsData = [
-        {
-            type: 'email',
-            name: 'email',
-            placeholder:'your email here'
-        },
-        {
-            type: 'password',
-            name: 'password',
-            placeholder:'your password here'
-        },
-        {
-            type: 'password',
-            name: 'confirm-password',
-            placeholder:'confirm password here'
-        }
-    ]
     const [loading, setLoading] = useState(true);
     useEffect(_=> {
         setLoading(true);
@@ -30,10 +15,14 @@ const SignUp = () => {
             setLoading(false);
         },500)
     },[dispatch])
+
+
+
+
     return (
         <>
             <Loading loading ={loading}/>
-            <AuthForm inputsData={inputsData} text='sign up'/>  
+            <AuthForm />  
         </>
     )
 }
