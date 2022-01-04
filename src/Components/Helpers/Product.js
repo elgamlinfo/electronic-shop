655286e17b55c784d05ac9d6485380ef0d1ab60c
elgamlinfo <elgaml.info@gmail.com>
2022-01-04 21:36:08 +0200

    favourite pages

--- src/Components/Helpers/Product.js
+++ src/Components/Helpers/Product.js
@@ -3,11 +3,60 @@ import './helpers.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus, faEye, faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux'
+
+
 const Product = (props) => {
     let formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
     });
+
+    const isAuth = useSelector(state => state.user.isAuth)
+
+    function addToFav(e) {
+        e.preventDefault();
+        if(!isAuth) return toast.warning("login or register please😊")
+        axios
+        .post(
+            `${process.env.REACT_APP_API_LINK_DEV}/fav/add`,
+            {prodId: e.currentTarget.id},
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`
+                },
+            }
+        )
+        .then(res => {
+            toast.success("product added to favourite successfully😊")
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    function deleteFromFav(e) {
+        e.preventDefault();
+        if(!isAuth) return toast.warning("login or register please😊")
+        props.removeFav(e.currentTarget.id)
+        axios
+        .delete(
+            `${process.env.REACT_APP_API_LINK_DEV}/fav/${e.currentTarget.id}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`
+                },
+            }
+        )
+        .then(res => {
+            toast.success("product removed favourite successfully😊")
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
 
     return (
         <div className='product'>
@@ -17,8 +66,8 @@ const Product = (props) => {
                     <Link to={`/product?id=${props.id}`}><FontAwesomeIcon icon={faEye}/></Link>
                     <Link to=''><FontAwesomeIcon icon={faCartPlus}/></Link>
                     {props.fav? 
-                        <Link to=''><FontAwesomeIcon icon={faHeartBroken}/></Link>: 
-                        <Link to=''><FontAwesomeIcon icon={faHeart}/></Link>
+                        <a href='*' id={props.id}  onClick={e => deleteFromFav(e)}><FontAwesomeIcon icon={faHeartBroken}/></a>: 
+                        <a href='*' id={props.id}  onClick={e => addToFav(e)}><FontAwesomeIcon icon={faHeart}/></a>
                     }
                 </div>
             </div>
--- src/Components/Profile/Favurite.js
+++ src/Components/Profile/Favurite.js
@@ -1,18 +1,63 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Product from '../Helpers/Product'
 import './favourite.scss'
+import axios from 'axios'
+import Loading from '../Loading/Loading'
+
+
+
 const Favurite = () => {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        axios
+        .get(
+            `${process.env.REACT_APP_API_LINK_DEV}/fav`,
+            {
+                headers: {
+                    Authorization: `Bearer ${localStorage.token}`
+                },
+            }
+        )
+        .then(res => {
+            setData(res.data)
+            setLoading(false)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, [])
+
+    function removeFav(id) {
+        setData(data => data.filter(prod => prod._id !== id))
+    }
+
     return (
-        <div className='favourite_content'>
-            <h2 className='titl'>Favourite</h2>
-            <div className='favourite_card_wrapper'>
-                <Product fav={true}/>
-                <Product fav={true}/>
-                <Product fav={true}/>
-                <Product fav={true}/>
-                <Product fav={true}/>
+        <>
+            {!data?<Loading loading={loading}/>:
+            <div className='favourite_content'>
+                <h2 className='titl'>Favourite</h2>
+                <div className='favourite_card_wrapper'>
+                    {data.map(prod =>{
+                        return(
+                            <Product 
+                                key={prod._id}
+                                title={prod.title}
+                                img={prod.images[0]}
+                                price={prod.price}
+                                id={prod._id}
+                                fav={true}
+                                removeFav={removeFav}
+                            />
+                        )
+                    } )}
+                    
+                </div>
             </div>
-        </div>
+            }
+        </>
     )
 }
 
